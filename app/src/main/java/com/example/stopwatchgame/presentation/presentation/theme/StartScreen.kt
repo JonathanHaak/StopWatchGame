208a61f8f5e9bcdec1706e9a072ad6bba8d5c0da
Jonathan Haak <haak0052@umn.edu>
2023-05-28 10:57:10 -0500

    New StartScreen UI

--- app/src/main/java/com/example/stopwatchgame/presentation/presentation/theme/StartScreen.kt
+++ app/src/main/java/com/example/stopwatchgame/presentation/presentation/theme/StartScreen.kt
@@ -1,51 +1,46 @@
 package com.example.stopwatchgame.presentation.presentation.theme
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.clickable
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
 fun StartScreen(navController: NavController) {
-       Surface(modifier = Modifier.fillMaxSize()){
-           Column(modifier = Modifier.fillMaxSize(),
-               verticalArrangement = Arrangement.SpaceEvenly,
-               horizontalAlignment = Alignment.CenterHorizontally){
-               Button(onClick = {
-                   navController.navigate("GameOne")
-               }){
-                   Text("GameOne")
-               }
+    LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
+        item {
+            CustomText(text = "GameOne", onClick = { navController.navigate("GameOne") }, color = Color.Red)
+        }
+        item {
+            CustomText(text = "GameTwo", onClick = { navController.navigate("GameTwo") }, color = Color.Green)
+        }
+        item {
+            CustomText(text = "GameThree", onClick = { navController.navigate("GameThree") }, color = Color.Blue)
+        }
+        item {
+            CustomText(text = "GameFour", onClick = { navController.navigate("GameFour") }, color = Color.Yellow)
+        }
+        item {
+            CustomText(text = "Info", onClick = { navController.navigate("Info") }, color = Color.Magenta)
+        }
+    }
+}
 
-               Button(onClick = {
-                   navController.navigate("GameTwo")
-               }){
-                   Text("GameTwo")
-               }
-
-               Button(onClick = {
-                   navController.navigate("GameThree")
-               }){
-                   Text("GameThree")
-               }
-
-               Button(onClick = {
-                   navController.navigate("GameFour")
-               }){
-                   Text("GameFour")
-               }
-               Button(onClick = {
-                   navController.navigate("Info")
-               }){
-                   Text("Info")
-               }
-           }
-       }
-
+@Composable
+fun CustomText(text: String, onClick: () -> Unit, color: Color) {
+    Text(
+        text = text,
+        style = TextStyle(color = color, fontSize = 50.sp, fontWeight = FontWeight.Bold),
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+    )
 }
